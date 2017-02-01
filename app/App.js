58524af42434469921bbb33b0286de1bdcee86c5
shuntaka <chang.shoon@gmail.com>
2017-02-01 22:33:25 +0900

    initial commit

---
+++ app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+// import YourComponent from './component/YourComponent';
+
+class App extends Component {
+  // render() {
+  //   return (
+  //     <div>
+  //       <YourComponent />
+  //     </div>
+  //   );
+  // }
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
