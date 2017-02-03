50623a97274697c61f3e307f20db421a7b8ed916
shuntaka <chang.shoon@gmail.com>
2017-02-04 06:31:19 +0900

    install eslint

--- app/App.js
+++ app/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
-// import YourComponent from './component/YourComponent';
+import { Button } from 'react-bootstrap';
+import '../bower_components/bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
-  // render() {
-  //   return (
-  //     <div>
-  //       <YourComponent />
-  //     </div>
-  //   );
-  // }
   render() {
     return (
       <div>
+        <Button bsStyle="primary">Primary</Button>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,11 @@ var config = {
       },
       {
         test: /\.css$/,
-        loader: "style!css?modules"
-      }
+        // loader: "style!css?modules"
+        loader: "style!css"
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   devServer: {
